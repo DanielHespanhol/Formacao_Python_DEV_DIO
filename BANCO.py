95e00df257bb7a2f088bed076ba81d449e111e36
DanielHespanhol <99699865+DanielHespanhol@users.noreply.github.com>
2023-06-04 22:23:07 -0300

    Adicionado via UpLoad

---
+++ BANCO.py
@@ -0,0 +1,69 @@
+menu = """
+================ INICIO ================
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+========================================
+Observações: 3 Saques diários liberados.
+Valor máximo para saque de R$500,00.
+========================================
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = int(input(menu))
+
+    if opcao == 1:
+        valor = float(input("Confirme o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação cancelada! O valor informado é inválido.")
+
+    elif opcao == 2:
+        valor = float(input("Digite o valor do saque: "))
+
+        sem_saldo = valor > saldo
+
+        sem_limite = valor > limite
+
+        sem_saques = numero_saques >= LIMITE_SAQUES
+
+        if sem_saldo:
+            print("Operação cancelada! Você não tem saldo suficiente.")
+
+        elif sem_limite:
+            print("Operação cancelada! O valor do saque excede o limite.")
+
+        elif sem_saques:
+            print("Operação cancelada! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação cancelada! O valor informado é inválido.")
+
+    elif opcao == 3:
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == 0:
+        break
+
+    else:
+        print("Operação cancelada! Digite uma opção válida.")
